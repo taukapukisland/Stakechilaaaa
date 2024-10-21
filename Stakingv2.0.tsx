022aff9d68752ccc4da1459d51c0e3a0dd3f297a
taukapukisland <youmaysenditto@gmail.com>
2024-10-22 02:54:16 +0800

    Add files via upload

---
+++ Stakingv2.0.tsx
@@ -0,0 +1,163 @@
+'use client';
+
+import { client } from "@/app/client";
+import { chain } from "@/app/chain";
+import { ConnectButton, TransactionButton, useActiveAccount, useReadContract } from "thirdweb/react";
+import { NFT_CONTRACT, STAKING_CONTRACT } from "../src/app/utils/contracts";
+import { NFT, prepareContractCall, sendTransaction, createThirdwebClient, getContract  } from "thirdweb";
+import { useEffect, useState } from "react";
+import { getNFTs, ownerOf, totalSupply, claimTo, isERC721, getOwnedNFTs, startTokenId } from "thirdweb/extensions/erc721";
+import { NFTCard } from "./NFTCard";
+import { getOwnedERC721s } from "./getOwnedERC721s";
+import { owner } from "thirdweb/modules";
+
+
+export const Staking = () => {
+    const account = useActiveAccount();
+
+
+    const [ownedNFTs, setOwnedNFTs] = useState<NFT[]>([]);
+   const getOWnedNFTs = async () => {
+
+    const contract = getContract({
+        client,
+        chain: chain,
+        address: "0x41860523F4e5C817ca397C4610Ddea36b8af5254",
+      });
+
+// ReactJS/NextJS
+const { data, error } = useReadContract(getOwnedERC721s, {
+	contract: NFT_CONTRACT,
+	owner: "Address",
+});
+
+// Typescript
+let ownedNFTs = await getOwnedERC721s({
+  contract: NFT_CONTRACT,
+	owner: "Address"
+});
+
+
+
+   }
+
+  
+   
+   const {
+    data: stakedInfo,
+    refetch: refetchStakedInfo
+} = useReadContract({
+    contract: STAKING_CONTRACT,
+    method: "getStakeInfo",
+    params: [account?.address || ""]
+});
+  /*
+    const getOwnedNFTs = async () => {
+        let ownedNFTs: NFT[] = [];
+
+        const totalNFTSupply = await totalSupply ({
+            contract: NFT_CONTRACT
+        });       
+        const nfts = await getOwnedERC721s({
+            contract: NFT_CONTRACT,
+        });
+        const { data, error } = useReadContract(getOwnedERC721s, {
+            contract: NFT_CONTRACT,
+            owner: "Address",
+        });
+        for(let nft of nfts){
+                const owner = await ownerOf({
+                    contract: NFT_CONTRACT,
+                    tokenId: nft.id,
+                });
+                if(owner === account?.address){
+                    ownedNFTs.push(nft);
+                }
+        }
+        setOwnedNFTs(ownedNFTs);
+    };
+
+    useEffect (() => {
+        if(account){
+                getOwnedERC721s();
+        }
+    }, [account]);
+
+    const {
+        data: stakedInfo,
+        refetch: refetchStakedInfo
+    } = useReadContract({
+        contract: STAKING_CONTRACT,
+        method: "getStakeInfo",
+        params: [account?.address || ""]
+    });
+
+   
+
+
+      
+    const Etransaction = prepareContractCall({
+        contract: NFT_CONTRACT,
+        method: "function transfer(address to, uint256 value)",
+        params: [account?.address || "", BigInt(0)],
+      }); 
+   /* const { transactionHash } = await sendTransaction({
+        account,
+        transaction: Etransaction,
+      });*/
+
+      
+
+
+    if(account){
+        return(
+            <div style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                backgroundColor: "#151515",
+                borderRadius: "8px",
+                width: "500px",
+                padding: "20px",
+            }}>
+                <ConnectButton
+                    client={client}
+                    chain={chain}
+                />
+                <div style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    alignItems: "center",
+                    justifyContent: "space-between",
+                    margin: "20px 0",
+                    width: "100%",
+                }}>
+                    <h2 style={{ marginRight: "20px",color: "white",}}>Your Chilaaaas!!!!</h2>
+                </div>
+                <hr style={{
+                    width: "100%",
+                    border: "1px solid #333"
+                }}/>
+                <div style={{
+                    margin: "20px 0",
+                    width: "100%"
+                }}>
+                    
+                    <div style={{display: "flex", flexDirection:"row", flexWrap: "wrap", width: "500px"}}>
+                        {ownedNFTs && ownedNFTs.length > 0 ? (
+                            ownedNFTs.map((nft) => (
+                                <NFTCard
+                                    key={nft.id}
+                                    nft={nft}
+                                    refetchOwnedNFTs={getOWnedNFTs}
+                                    refetchStakedInfo={refetchStakedInfo}
+                                />
+                            ))
+                        ) : (
+                            <p style={{color: "white"}}>You own 0 NFTs</p>
+                        )}
+                </div>
+            </div></div>
+        )    
+}
+} 
